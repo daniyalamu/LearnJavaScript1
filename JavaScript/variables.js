5d777db1a3735066027690f770d801b48a9803a3
MD DANIYAL HASAN <117173402+daniyalamu@users.noreply.github.com>
2023-10-26 20:04:56 +0000

    variables

---
+++ JavaScript/variables.js
@@ -0,0 +1,13 @@
+const classId= 1234
+let subject = 'computer'
+var course ="BTech"
+place = 'katihar'
+// classId=3 we can't do this becouse it is const veriables we can not change class id
+
+console.log(classId);
+let rollNo;
+console.table([classId, subject, course, place, rollNo])
+console.log(typeof undefined)// data type of undefined
+console.log(typeof null)   //data of null is object 
+
+
