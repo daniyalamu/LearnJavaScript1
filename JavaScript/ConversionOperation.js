a14d645d5cb550cf6a941051ed075bf119310c33
MD DANIYAL HASAN <117173402+daniyalamu@users.noreply.github.com>
2023-10-26 21:37:01 +0000

    add some operation

--- JavaScript/ConversionOperation.js
+++ JavaScript/ConversionOperation.js
@@ -19,3 +19,15 @@ console.log(typeof stringToNum)         // but data type is number
 // number to boolean 
 console.log(NumToBoolean)
 console.log(typeof NumToBoolean)
+
+// ++++++++++++++++++++++++++++++++++++++++operation ++++++++++++++++++++++++++++
+let str1="hello"
+let str2 = " daniyal"
+let str3 = str1 + str2
+console.log(str3)
+
+console.log(1+2+"3") // output will be 33 (1+2)+"3"=3+"3"="33"=33
+console.log("1"+2+3)  // output will be 123  if first is string then all are consider as string
+
+console.log(true)  // output will be true
+console.log(+true) // output will be 1
