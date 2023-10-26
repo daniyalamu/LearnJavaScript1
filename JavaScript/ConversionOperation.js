83603f681e52ca48e9806b9416f992f2dcf2b02c
MD DANIYAL HASAN <117173402+daniyalamu@users.noreply.github.com>
2023-10-26 20:58:17 +0000

    conversion operation

---
+++ JavaScript/ConversionOperation.js
@@ -0,0 +1,21 @@
+let Id=33;
+let name1 ="daniyal"
+let isLoggedIn = 1
+
+
+let idToString=String(Id)
+let stringToNum = Number(name1)
+let NumToBoolean = Boolean(isLoggedIn)
+
+
+// number to string 
+ console.log(idToString)
+ console.log(typeof idToString)
+
+// string to number 
+console.log(stringToNum)                 // NaN Not a Number 
+console.log(typeof stringToNum)         // but data type is number 
+
+// number to boolean 
+console.log(NumToBoolean)
+console.log(typeof NumToBoolean)
